1188a48e543e7781cc3809936134702a759356b0
krulik <iwillkissyou@gmail.com>
2017-05-14 17:52:14 +0300

    add basic echo server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('foo bar'); 
+var http = require('http');
+var server = http.createServer();
+server.listen(process.env.PORT);
+server.on('request', handleRequest);
+
+function handleRequest (request, response) {
+  response.end(`Handling request URL: ${request.url}`);
+}
+
+console.log(`listening on ${process.env.PORT}`); 
