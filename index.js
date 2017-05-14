044f230dea9bdfa87e316c0d4a5f670be0baa300
krulik <iwillkissyou@gmail.com>
2017-05-14 18:12:50 +0300

    add mongo

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 var http = require('http');
+var mongo = require('mongodb').MongoClient;
+
 var server = http.createServer();
 server.listen(process.env.PORT);
 server.on('request', handleRequest);
+
+var url = 'mongodb://heroku_f9src7jj:@ds133981.mlab.com:33981/heroku_f9src7jj';
+
+mongo.connect(url, function (err, _db) {
+  console.log('Connected successfully to mongo');
+  console.log(_db);
+//   db = _db;
+//   collection = db.collection('messages');
+//   collection.find({}).toArray(function (err, docs) {
+//     console.log(docs);
+//     messages = docs;
+//   });
+});
 
 function handleRequest (request, response) {
   response.end(`Handling request URL: ${request.url}`);
