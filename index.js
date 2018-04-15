4c5d047e3f55601da6ea402dc0f7fd3d72709af8
krulik <iwillkissyou@gmail.com>
2018-04-15 21:37:55 +0300

    change text again

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function handleRequest (request, response) {
       response.end(data);
     });
   } else {
-    response.end(`Hello URL: ${request.url}, dbName: ${db.databaseName}`);
+    response.end(`Hi URL: ${request.url}, dbName: ${db.databaseName}`);
   }
 }
 
