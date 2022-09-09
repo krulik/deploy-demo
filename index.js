588d07411bdd52f3fef617efa2ea84e21e9e3b81
krulik <iwillkissyou@gmail.com>
2022-09-09 11:55:17 +0300

    refactor with express

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 'use strict';
 
-let http = require('http');
 let fs = require('fs');
+
+const express = require('express');
+var cors = require('cors');
+const app = express();
+app.use(cors());
+
 let mongo = require('mongodb').MongoClient;
 
-let url = process.env.MONGO_URL || '';
+let url = process.env.MONGO_URL || 'mongodb://heroku-demo:jKcmGj5mJlClgVE4@cluster0-shard-00-00.okzaw.gcp.mongodb.net:27017,cluster0-shard-00-01.okzaw.gcp.mongodb.net:27017,cluster0-shard-00-02.okzaw.gcp.mongodb.net:27017/demo?ssl=true&replicaSet=atlas-vmfgkh-shard-0&authSource=admin&retryWrites=true&w=majorityvmongodb://heroku-demo:jKcmGj5mJlClgVE4@cluster0-shard-00-00.okzaw.gcp.mongodb.net:27017,cluster0-shard-00-01.okzaw.gcp.mongodb.net:27017,cluster0-shard-00-02.okzaw.gcp.mongodb.net:27017/demo?ssl=true&replicaSet=atlas-vmfgkh-shard-0&authSource=admin&retryWrites=true&w=majorityv';
 let db, collection;
-let server = http.createServer();
-server.listen(process.env.PORT || '5000');
-server.on('request', handleRequest);
+
 if (url) {
   connectToMongo(url);
 }
@@ -22,28 +25,26 @@ function connectToMongo(url) {
     db = _db;
     console.log('Connected successfully to mongo', db);
     collection = db.collection('items');
-    collection.find({data: 42}).toArray(function (err, docs) {
-      if (err) {
-        console.log(`got query error=[${err}]`);
-        return;
-      }
-      console.log('got docs', docs);
-    });
   });
 }
 
-function handleRequest (request, response) {
-  if (request.url === '/' || request.url === '/index.html') {
-    fs.readFile('index.html', 'utf-8', (err, data) => {
-      response.end(data);
-    });
-  } else {
-    response.end(`Hi 2022 URL: ${request.url}, dbName: ${db.databaseName}`);
-  }
-}
+const port = process.env.PORT || '5000';
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
 
-console.log(`listening on ${process.env.PORT}`);
+app.get('/', (req, res) => {
+  fs.readFile('index.html', 'utf-8', (err, data) => {
+    res.send(data);
+  });
+});
 
-module.exports = {
-  handleRequest
-};
+app.get('/docs', (req, res) => {
+  collection.find({data: 42}).toArray(function (err, docs) {
+    if (err) {
+      console.log(`got query error=[${err}]`);
+      return;
+    }
+    res.json(docs);
+  });
+});
